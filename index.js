366e47c6f8927870dee007c774041c457f8778e6
Zhenchao Yan <yanzhenchao@gmail.com>
2014-12-11 20:25:01 +0000

    modified

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs'); // new
+var filename = 'index.html'; // new
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Fuck You');
+    var html = fs.readFileSync(filename).toString();
+    response.send(html);
 });
 
 app.listen(app.get('port'), function() {
