32ee9fb1e33be39bcb1efd497cf8bd96cc3449be
Zhenchao Yan <yanzhenchao@gmail.com>
2014-12-25 03:34:38 +0000

    index.js revised

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs = require('fs');
+var filename = 'index.html';
+var http = require('http');
+
 var app = express();
-var fs = require('fs'); // new
-var filename = 'index.html'; // new
+
+app.get('/', function(request, response){
+    var html = fs.readFileSync(filename).toString();
+    response.send(html);
+});
+
+var port = process.env.PORT || 8080;
+
+http.createServer(app).listen(port, function(){
+    console.log("Listen on " + port);
+});
+
+
+// The new one
+
+/*
+
+var app = express();
 
 app.set('port', (process.env.PORT || 8080));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
@@ -12,5 +33,11 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("Listening on " + app.get('port'));
 });
+
+*/
+// The old one
+
+
+
