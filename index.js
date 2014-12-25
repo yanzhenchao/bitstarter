b82971037dd81eb7c59ffeab5e2f1c98da2e5dce
Zhenchao Yan <yanzhenchao@gmail.com>
2014-12-25 03:59:19 +0000

    restored index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var filename = 'index.html';
+var http = require('http');
+
 var app = express();
-var fs = require('fs'); // new
-var filename = 'index.html'; // new
 
-app.set('port', (process.env.PORT || 8080));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
+app.get('/', function(request, response){
     var html = fs.readFileSync(filename).toString();
     response.send(html);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+var port = process.env.PORT || 8080;
+
+http.createServer(app).listen(port, function(){
+    console.log("Listen on " + port);
 });
